6ef451775eb4666e8afe4b77c5152bfd02532b0b
Aidan Feldman <aidan.feldman@gmail.com>
2014-02-05 02:53:12 -0200

    a bit of test cleanup

--- test/markdown_test.rb
+++ test/markdown_test.rb
@@ -4,32 +4,30 @@ require 'action_view'
 require_relative '../lib/html_pipeline_rails'
 
 describe "markdown views" do
-  TEMPLATE_PATH = File.join(File.dirname(__FILE__), 'templates')
-
   def setup
     @view = ActionView::Base.new
-    @view.view_paths << TEMPLATE_PATH
+    @view.view_paths << File.join(File.dirname(__FILE__), 'templates')
   end
 
   it "inline rendering" do
     template = '# A Heading'
-    result = @view.render inline: template, type: :md
+    result = @view.render(inline: template, type: :md)
     result.must_equal('<h1>A Heading</h1>')
   end
 
   it "doesn't affect normal HTML rendering" do
     template = '# A Heading'
-    result = @view.render inline: template
+    result = @view.render(inline: template)
     result.must_equal(template)
   end
 
   it "renders external templates" do
-    result = @view.render 'simple_md'
+    result = @view.render('simple_md')
     result.must_equal("<h1>A Heading</h1>")
   end
 
   it "doesn't affect normal external HTML templates" do
-    result = @view.render 'fake_md'
+    result = @view.render('fake_md')
     result.must_equal("# Fake Heading\n")
   end
 end
