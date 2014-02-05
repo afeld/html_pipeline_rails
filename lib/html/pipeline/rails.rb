40843e7f2d92b7d02fcf7de354aaf8f001474a58
Aidan Feldman <aidan.feldman@gmail.com>
2014-02-04 23:20:50 -0200

    generate gem from bundler

---
+++ lib/html/pipeline/rails.rb
@@ -0,0 +1,9 @@
+require "html/pipeline/rails/version"
+
+module Html
+  module Pipeline
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/html/pipeline/rails/version.rb
@@ -0,0 +1,7 @@
+module Html
+  module Pipeline
+    module Rails
+      VERSION = "0.0.1"
+    end
+  end
+end
