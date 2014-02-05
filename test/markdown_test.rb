0be77c215b7e55a9fc3357d4a10c7af767c89f0b
Aidan Feldman <aidan.feldman@gmail.com>
2014-02-05 02:52:04 -0200

    add tests for external templates

--- test/markdown_test.rb
+++ test/markdown_test.rb
@@ -4,8 +4,11 @@ require 'action_view'
 require_relative '../lib/html_pipeline_rails'
 
 describe "markdown views" do
+  TEMPLATE_PATH = File.join(File.dirname(__FILE__), 'templates')
+
   def setup
     @view = ActionView::Base.new
+    @view.view_paths << TEMPLATE_PATH
   end
 
   it "inline rendering" do
@@ -18,5 +21,15 @@ describe "markdown views" do
     template = '# A Heading'
     result = @view.render inline: template
     result.must_equal(template)
+  end
+
+  it "renders external templates" do
+    result = @view.render 'simple_md'
+    result.must_equal("<h1>A Heading</h1>")
+  end
+
+  it "doesn't affect normal external HTML templates" do
+    result = @view.render 'fake_md'
+    result.must_equal("# Fake Heading\n")
   end
 end
