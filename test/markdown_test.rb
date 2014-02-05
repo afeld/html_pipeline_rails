033b9daed227f76f067584d1000de728239f143d
Aidan Feldman <aidan.feldman@gmail.com>
2014-02-05 01:31:30 -0200

    convert test to minitest syntax

--- test/markdown_test.rb
+++ test/markdown_test.rb
@@ -1,13 +1,15 @@
+require 'minitest/spec'
+
 require 'action_view/test_case'
 require_relative '../lib/html_pipeline_rails'
 
 class MarkdownTest < ActionView::TestCase
-  def test_inline_rendering
+  test "inline rendering" do
     template = '# A Heading'
     result = '<h1>A Heading</h1>'
 
     render inline: template, type: :md
 
-    assert_equal(result, rendered)
+    result.must_equal(rendered)
   end
 end
